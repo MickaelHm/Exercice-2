1d225f812709e0802c5a407d21550e3e2c777a8d
MickaelHm <harmeau.mickael@gmail.com>
2018-12-17 14:44:29 +0100

    Exercice complet

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            Console.WriteLine("Les valeurs x, y et z sont respectivement: " + x + ", " + y + " et " + z);
+            int somme = (x + y);
+            int produit =((x + y) * z);
+            Console.WriteLine("(x+y)*z = " +produit);
+
+
+          
+
+           
+        }
+    }
+}
